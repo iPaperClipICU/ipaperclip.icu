34efa9b88c22547979aab50bb08a9459190e77e0
ipaperclipIcu <91960978+ipaperclipIcu@users.noreply.github.com>
2021-10-05 21:43:53 +0800

    初始化

---
+++ assets/js/干燥工厂_config.js
@@ -0,0 +1,20 @@
+var config = {
+    "All": {
+        "ID1c6a22572396a163e75d6b6fd39fba02": {
+            "name": "提前动手 Vol.001 我们做了三面镜子：挺钢的，有点硬.mp4",
+            "id": "ID1c6a22572396a163e75d6b6fd39fba02",
+            "url": "https://file.hsyhx.top/干燥工厂/提前动手 Vol.001 我们做了三面镜子：挺钢的，有点硬.mp4",
+            "type": "video",
+            "time": "2021.09.27",
+            "size": "39.38 MB"
+        },
+        "IDde66db86d6d5664fe15ad2513a56d128": {
+            "name": "提前动手 Vol.002 我们做了个公开成本的 T 恤套装.mp4",
+            "id": "IDde66db86d6d5664fe15ad2513a56d128",
+            "url": "https://file.hsyhx.top/干燥工厂/提前动手 Vol.002 我们做了个公开成本的 T 恤套装.mp4",
+            "type": "video",
+            "time": "2021.09.27",
+            "size": "40.98 MB"
+        }
+    }
+}
---
+++ assets/js/示例2_config.js
@@ -0,0 +1,38 @@
+var config = {
+    "Demo1": {
+        "video_1_1": {
+            "name": "一级标签_第一个测试视频.mp4",
+            "id": "video_1_1",
+            "url": "/",
+            "type": "video",
+            "time": "2021.09.26",
+            "size": "39.38 MB"
+        },
+        "video_1_2": {
+            "name": "一级标签_第二个测试视频.mp4",
+            "id": "video_1_2",
+            "url": "/",
+            "type": "video",
+            "time": "2021.09.26",
+            "size": "40.98 MB"
+        }
+    },
+    "Demo2": {
+        "video_2_1": {
+            "name": "二级标签_第一个测试视频.mp4",
+            "id": "video_2_1",
+            "url": "/",
+            "type": "video",
+            "time": "2021.09.26",
+            "size": "39.38 MB"
+        },
+        "video_2_2": {
+            "name": "二级标签_第二个测试视频.mp4",
+            "id": "video_2_2",
+            "url": "/",
+            "type": "video",
+            "time": "2021.09.26",
+            "size": "40.98 MB"
+        }
+    }
+}
