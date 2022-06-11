5ef953308b59644520577d13523f0f9c39edf459
qiaoshouzi <qiaoshouzi@hgy.ooo>
2022-06-12 01:11:32 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: "all",
+  },
 });
