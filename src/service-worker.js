4751af425e2d2395569c0b83e9ebc83f82138ff4
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-06-21 23:20:26 +0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -22,7 +22,7 @@ workbox.core.skipWaiting(); // 强制等待中的 Service Worker 被激活
 workbox.core.clientsClaim(); // Service Worker 被激活后使其立即获得页面控制权
 
 // vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 // Other
