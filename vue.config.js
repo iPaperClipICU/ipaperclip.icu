aeaf30cc65cfdd68273612a8871f3c0caf6c5ee1
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-06-27 07:35:29 +0800

    del PWA

--- vue.config.js
+++ vue.config.js
@@ -4,17 +4,4 @@ module.exports = defineConfig({
   devServer: {
     allowedHosts: "all",
   },
-  pwa: {
-    name: "ipaperclip.icu",
-    // 背景色
-    themeColor: "#101014",
-    // msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
-    // configure the workbox plugin (GenerateSW or InjectManifest)
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      swSrc: "./src/service-worker.js",
-    },
-  },
 });
