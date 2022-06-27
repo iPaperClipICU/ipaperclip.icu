9670f76262d2568fa2bbbc0759272094ebd94d1e
qiaoshouzi <qiaoshouzi@hgy.ooo>
2022-06-28 07:15:13 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.unregister();
+  });
+}
+
 createApp(App).use(router).mount("#app");
