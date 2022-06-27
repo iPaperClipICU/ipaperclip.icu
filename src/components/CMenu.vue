0e124e9b80e72e7b0c7258fc44cd259660368736
qiaoshouzi <qiaoshouzi@hgy.ooo>
2022-06-28 07:08:17 +0800

    重构UI

---
+++ src/components/CMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <n-scrollbar x-scrollable>
+    <div style="white-space: nowrap; padding: 12px">
+      <slot />
+    </div>
+  </n-scrollbar>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { NScrollbar } from "naive-ui";
+
+export default defineComponent({
+  components: { NScrollbar },
+});
+</script>
