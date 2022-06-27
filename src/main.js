5f1df3d1842c57b8512765c15ee4159e3d9255b4
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-06-28 07:18:03 +0800

    注销 Service Worker

--- src/main.js
+++ src/main.js
@@ -2,10 +2,4 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.ready.then((registration) => {
-    registration.unregister();
-  });
-}
-
 createApp(App).use(router).mount("#app");
