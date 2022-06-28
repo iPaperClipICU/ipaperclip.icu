6df224138b54e95346fab9c97dc9007a9deb4d38
qiaoshouzi <qiaoshouzi@cfm.moe>
2022-06-28 10:34:21 +0800

    新增搜索功能

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <n-config-provider :locale="zhCN" :theme="darkTheme">
-    <n-dialog-provider>
-      <n-loading-bar-provider>
-        <div style="text-align: center; padding-top: 15px">
-          <n-h1>
-            <a style="color: #ffffffe6; text-decoration: none" href="/">
-              iPaperClipICU
-            </a>
-          </n-h1>
-        </div>
-        <n-grid :cols="36" item-responsive style="padding-top: 20px">
-          <n-gi span="1 1330:3" />
-          <n-gi span="34 1330:30">
-            <router-view />
-            <div style="padding-top: 15px">
-              <READMECard />
-            </div>
-          </n-gi>
-          <n-gi span="1 1330:3" />
-        </n-grid>
-      </n-loading-bar-provider>
-    </n-dialog-provider>
+    <n-loading-bar-provider>
+      <div style="text-align: center; padding-top: 15px">
+        <n-h1>
+          <a style="color: #ffffffe6; text-decoration: none" href="/">
+            iPaperClipICU
+          </a>
+        </n-h1>
+      </div>
+      <n-grid :cols="36" item-responsive style="padding-top: 20px">
+        <n-gi span="1 1330:3" />
+        <n-gi span="34 1330:30">
+          <router-view />
+          <div style="padding-top: 15px">
+            <READMECard />
+          </div>
+        </n-gi>
+        <n-gi span="1 1330:3" />
+      </n-grid>
+    </n-loading-bar-provider>
     <n-global-style />
   </n-config-provider>
 </template>
@@ -29,12 +27,7 @@
 import { defineComponent, ref } from "vue";
 import READMECard from "@/components/READMECard";
 import { NH1, NGi, NGrid } from "naive-ui";
-import {
-  NGlobalStyle,
-  NConfigProvider,
-  NDialogProvider,
-  NLoadingBarProvider,
-} from "naive-ui";
+import { NGlobalStyle, NConfigProvider, NLoadingBarProvider } from "naive-ui";
 import { darkTheme, zhCN } from "naive-ui";
 
 const mailLoginShow = ref(true);
@@ -46,7 +39,6 @@ export default defineComponent({
     NGrid,
     NGlobalStyle,
     NConfigProvider,
-    NDialogProvider,
     NLoadingBarProvider,
   },
   setup() {
