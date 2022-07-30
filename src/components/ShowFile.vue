4bbfd30356be3d0e282caea8421d7d5d4b22a6bc
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-07-30 11:00:01 +0800

    优化图片显示

--- src/components/ShowFile.vue
+++ src/components/ShowFile.vue
@@ -1,4 +1,7 @@
 <template>
+  <n-alert v-if="loadError" type="error">
+    {{ loadError_name }}加载失败
+  </n-alert>
   <!-- TODO: 写个视频/音频播放器 -->
   <div v-if="fineData.type == 'video'" class="video">
     <video :src="fineData.url" controls preload>
@@ -10,7 +13,7 @@
     <n-grid :cols="36" item-responsive>
       <n-gi span="1 768:5" />
       <n-gi span="34 768:26">
-        <img :src="fineData.url" :alt="fineData.name" style="width: 100%" />
+        <img :src="fineData.url" :alt="fineData.name" loading="lazy" style="width: 100%" :onerror="imgError(fineData)" />
       </n-gi>
       <n-gi span="1 768:5" />
     </n-grid>
@@ -25,16 +28,19 @@
 </template>
 
 <script>
-import { onMounted, defineComponent } from "vue";
-import { NGi, NGrid, NResult } from "naive-ui";
+import { ref, onMounted, defineComponent } from "vue";
+import { NGi, NGrid, NAlert, NResult } from "naive-ui";
 import "aplayer/dist/APlayer.min.css";
 import APlayer from "aplayer";
 import { getFileInfo } from "@/assets/box.js";
 
+const loadError = ref(false);
+const loadError_name = ref('额');
 export default defineComponent({
   components: {
     NGi,
     NGrid,
+    NAlert,
     NResult,
   },
   props: ["data"],
@@ -53,7 +59,13 @@ export default defineComponent({
       }
     });
     return {
+      loadError,
+      loadError_name,
       fineData: props.data,
+      imgError: () => {
+        loadError_name.value = "图片";
+        loadError.value = true;
+      },
     };
   },
 });
