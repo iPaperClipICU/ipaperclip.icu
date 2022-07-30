22e389bf1266e6a58b70296b29ba70f91f1b1845
qiaoshouzi <qiaoshouzi@hgy.ooo>
2022-07-30 10:31:21 +0800

    优化 FilesMenu 的多页功能

--- src/components/FilesMenu.vue
+++ src/components/FilesMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-list>
+  <n-list :key="listKey">
     <n-list-item v-for="(item, index) in getListData(data)" :key="index">
       <n-button
         size="large"
@@ -10,12 +10,7 @@
       >
         <template #icon>
           <n-icon>
-            <!-- Video -->
-            <svg v-if="item.type == 'video'" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 384 512"><path d="M369.941 97.941l-83.882-83.882A48 48 0 0 0 252.118 0H48C21.49 0 0 21.49 0 48v416c0 26.51 21.49 48 48 48h288c26.51 0 48-21.49 48-48V131.882a48 48 0 0 0-14.059-33.941zM332.118 128H256V51.882L332.118 128zM48 464V48h160v104c0 13.255 10.745 24 24 24h104v288H48zm228.687-211.303L224 305.374V268c0-11.046-8.954-20-20-20H100c-11.046 0-20 8.954-20 20v104c0 11.046 8.954 20 20 20h104c11.046 0 20-8.954 20-20v-37.374l52.687 52.674C286.704 397.318 304 390.28 304 375.986V264.011c0-14.311-17.309-21.319-27.313-11.314z" fill="currentColor"></path></svg>
-            <!-- Audio -->
-            <svg v-if="item.type == 'audio'" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 384 512"><path d="M369.941 97.941l-83.882-83.882A48 48 0 0 0 252.118 0H48C21.49 0 0 21.49 0 48v416c0 26.51 21.49 48 48 48h288c26.51 0 48-21.49 48-48V131.882a48 48 0 0 0-14.059-33.941zM332.118 128H256V51.882L332.118 128zM48 464V48h160v104c0 13.255 10.745 24 24 24h104v288H48zm144-76.024c0 10.691-12.926 16.045-20.485 8.485L136 360.486h-28c-6.627 0-12-5.373-12-12v-56c0-6.627 5.373-12 12-12h28l35.515-36.947c7.56-7.56 20.485-2.206 20.485 8.485v135.952zm41.201-47.13c9.051-9.297 9.06-24.133.001-33.439c-22.149-22.752 12.235-56.246 34.395-33.481c27.198 27.94 27.212 72.444.001 100.401c-21.793 22.386-56.947-10.315-34.397-33.481z" fill="currentColor"></path></svg>
-            <!-- Image -->
-            <svg v-if="item.type == 'image'" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 384 512"><path d="M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zM332.1 128H256V51.9l76.1 76.1zM48 464V48h160v104c0 13.3 10.7 24 24 24h104v288H48zm32-48h224V288l-23.5-23.5c-4.7-4.7-12.3-4.7-17 0L176 352l-39.5-39.5c-4.7-4.7-12.3-4.7-17 0L80 352v64zm48-240c-26.5 0-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48s-21.5-48-48-48z" fill="currentColor"></path></svg>
+            <FilesMenuICON :type="item.type" />
           </n-icon>
         </template>
         {{ item.name }}
@@ -33,7 +28,9 @@
 
 <script>
 import { ref, defineComponent } from "vue";
+import router from "@/router";
 import { NIcon, NList, NButton, NListItem, NPagination } from "naive-ui";
+import FilesMenuICON from "@/components/FilesMenuICON.vue";
 import { getFileInfo, getSearch } from "@/assets/box.js";
 
 /**
@@ -63,6 +60,8 @@ const getListData = (data) => {
   }
   const pages = data.data.length != 1;
   data = data.data;
+
+  console.log("test", getPage());
 
   if (pages) {
     showPage.value = true;
@@ -114,8 +113,10 @@ const getListData_search = (data) => {
 
 const showPage = ref(false);
 const maxPage = ref(100);
+const listKey = ref(1);
 export default defineComponent({
   components: {
+    FilesMenuICON,
     NIcon,
     NList,
     NButton,
@@ -128,9 +129,11 @@ export default defineComponent({
       getListData,
       showPage,
       maxPage,
+      listKey,
       getPage: getPage(),
-      updatePage(page) {
-        location.href = `${location.pathname}?p=${page}`;
+      async updatePage(page) {
+        await router.push(`${location.pathname}?p=${page}`);
+        listKey.value = page;
       },
     };
   },
