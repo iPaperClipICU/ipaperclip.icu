de689a3543f8b62c1055edab54720968d06c7c43
qiaoshouzi <qiaoshouzi@cfm.moe>
2022-08-09 03:42:32 +0800

    修改 style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <n-config-provider :locale="zhCN" :theme="darkTheme">
     <n-back-top />
-    <div style="text-align: center; padding-top: 15px">
+    <div style="text-align: center; margin-top: 15px">
       <n-h1>
         <a style="color: #ffffffe6; text-decoration: none" href="/">
           iPaperClipICU
         </a>
       </n-h1>
     </div>
-    <n-grid :cols="36" item-responsive style="padding-top: 15px">
+    <n-grid :cols="36" item-responsive style="margin-top: 15px">
       <n-gi span="1 1330:3" />
       <n-gi span="34 1330:30">
         <router-view />
