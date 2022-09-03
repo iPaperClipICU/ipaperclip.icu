b0e0f6d06f8e4df217c9a87e0e58eee4fdaa2b34
qiaoshouzi <qiaoshouzi@cfm.moe>
2022-09-03 17:01:27 +0800

    使用&适配Vite / Yarn

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
-import App from "@/App.vue";
-import router from "@/router";
-import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.mount("#app");
