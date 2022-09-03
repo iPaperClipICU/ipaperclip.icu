f13a58797e2e529d3ba55c468c9abd91ed2aeea1
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-09-03 17:45:18 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { createHtmlPlugin } from "vite-plugin-html";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
