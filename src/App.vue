898340c8ffa924e5b772bdc2b2b2ecb8efb86958
qiaoshouzi <qiaoshouzi@hgy.ooo>
2022-10-02 16:31:36 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <n-config-provider :locale="zhCN" :theme="darkTheme">
     <n-back-top />
-    <div style="text-align: center; margin-top: 15px">
-      <n-h1>
-        <a style="color: #ffffffe6; text-decoration: none" href="/">
-          iPaperClipICU
-        </a>
-      </n-h1>
+    <div :style="container">
+      <div style="text-align: center">
+        <n-h1>
+          <a style="color: #ffffffe6; text-decoration: none" href="/">
+            iPaperClipICU
+          </a>
+        </n-h1>
+      </div>
+      <router-view />
     </div>
-    <n-grid :cols="36" item-responsive style="margin-top: 15px">
-      <n-gi span="1 1330:3" />
-      <n-gi span="34 1330:30">
-        <router-view />
-      </n-gi>
-      <n-gi span="1 1330:3" />
-    </n-grid>
     <n-global-style />
   </n-config-provider>
 </template>
 
 <script setup>
-import { NGi, NH1, NGrid, NBackTop } from "naive-ui";
+import { NH1, NBackTop } from "naive-ui";
 import { zhCN, darkTheme, NGlobalStyle, NConfigProvider } from "naive-ui"; // NaiveUI Config
+
+// container 样式
+const container = {
+  "max-width": "1055px",
+  padding: "15px 15px",
+  margin: "0 auto",
+};
 </script>
