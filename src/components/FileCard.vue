4a61be147785e81f7b834658736f87238d1a6898
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-01-18 00:18:10 +0800

    ?

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- TODO: 写个视频/音频播放器 -->
   <div v-if="store.state.FileCardData.type === 'video'" class="video">
-    <video :src="store.state.FileCardData.url" controls preload>
+    <video :src="getSigh(store.state.FileCardData.url)" controls preload>
       <n-result status="info" title="您的浏览器不支持 video 标签" />
     </video>
   </div>
@@ -11,7 +11,7 @@
       <n-gi span="1 768:5" />
       <n-gi span="34 768:26">
         <img
-          :src="store.state.FileCardData.url"
+          :src="getSigh(store.state.FileCardData.url)"
           :alt="store.state.FileCardData.name"
           loading="lazy"
           style="width: 100%"
@@ -22,7 +22,7 @@
   </div>
   <div v-if="store.state.FileCardData.type === 'audio'">
     <!-- <audio preload="none" controls>
-      <source :src="store.state.FileCardData.url" type="audio/mpeg" />
+      <source :src="getSigh(store.state.FileCardData.url)" type="audio/mpeg" />
       <n-result status="info" title="您的浏览器不支持此音频格式" />
     </audio> -->
     <div id="audioPlayer"></div>
@@ -34,6 +34,8 @@ import { useStore } from "vuex";
 import { onMounted } from "vue";
 import APlayer from "aplayer";
 import "aplayer/dist/APlayer.min.css";
+import md5 from "js-md5";
+import { nanoid } from "nanoid";
 import { NGi, NGrid, NResult } from "naive-ui";
 import { getFileInfo } from "@/assets/utils.js";
 
@@ -46,13 +48,44 @@ onMounted(() => {
       audio: [
         {
           name: getFileInfo(store.state.FileCardData.name).name,
-          url: store.state.FileCardData.url,
+          url: getSigh(store.state.FileCardData.url),
         },
       ],
     });
     window.$AudioPlayer = ap;
   }
 });
+
+const getUID = () => {
+  nanoid(10);
+  let uid = localStorage.getItem("uid");
+  if (uid === null) {
+    const t = nanoid(10);
+    localStorage.setItem("uid", t);
+    uid = t;
+  }
+
+  return uid;
+};
+
+const getSigh = (FileURL) => {
+  const u = new URL(FileURL);
+  if (u.host === "ipaperclip-file.xodvnm.cn") {
+    const PKEY = import.meta.env.TencentCDN_PKEY || "null";
+    const uri = u.pathname; // url
+    const ts = Date.now() / 1000; // ts
+    const uid = getUID();
+    const rand = nanoid(10);
+    const sigh = `${ts}-${rand}-${uid}-${md5(
+      `${uri}-${ts}-${rand}-${uid}-${PKEY}`
+    )}`;
+    u.searchParams.set("sigh", sigh);
+
+    return u.href;
+  } else {
+    return FileURL;
+  }
+};
 </script>
 
 <style>
