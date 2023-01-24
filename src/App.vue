a7e8464ae5bca0b2d075b79310bf9e31ca886697
qiaoshouzi <qiaoshouzi@hgy.ooo>
2023-01-24 22:05:15 +0800

    Revert "Update App.vue"
    This reverts commit 641e44790a26ab63476bc1d3a4304c6bcd1ba3b0.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,44 @@
 <template>
   <n-config-provider :locale="zhCN" :theme="darkTheme">
     <n-back-top />
-    <n-layout position="absolute">
-      <n-layout-content :native-scrollbar="false" position="absolute">
-        <div class="container">
-          <!-- Title -->
-          <div
-            style="text-align: center; margin-top: 15px; margin-bottom: 10px"
-          >
-            <n-button text @click="router.push(`/`)">
-              <n-h1>iPaperClipICU</n-h1>
-            </n-button>
-          </div>
-          <!-- 搜索 -->
-          <n-card hoverable>
-            <n-grid :cols="4" item-responsive>
-              <n-gi span="4 425:2 705:1">
-                <n-input-group>
-                  <n-input
-                    v-model:value="searchValue"
-                    placeholder="搜索"
-                    clearable
-                  />
-                  <n-button type="primary" @click="searchButtonClick" ghost>
-                    搜索
-                  </n-button>
-                </n-input-group>
-              </n-gi>
-              <n-gi span="0 425:2 705:3" />
-            </n-grid>
-          </n-card>
-          <div style="margin-top: 15px">
-            <n-card hoverable>
-              <!-- 菜单 -->
-              <TagMenu />
-              <n-divider />
-              <router-view />
-            </n-card>
-          </div>
-          <!-- README -->
-          <div v-if="showREADME" style="margin-top: 15px">
-            <READMECard />
-          </div>
-        </div>
-      </n-layout-content>
-    </n-layout>
+    <div class="container">
+      <!-- Title -->
+      <div style="text-align: center; margin-top: 15px; margin-bottom: 10px">
+        <n-button text @click="router.push(`/`)">
+          <n-h1>iPaperClipICU</n-h1>
+        </n-button>
+      </div>
+      <!-- 搜索 -->
+      <n-card hoverable>
+        <n-grid :cols="4" item-responsive>
+          <n-gi span="4 425:2 705:1">
+            <n-input-group>
+              <n-input
+                v-model:value="searchValue"
+                placeholder="搜索"
+                clearable
+              />
+              <n-button type="primary" @click="searchButtonClick" ghost>
+                搜索
+              </n-button>
+            </n-input-group>
+          </n-gi>
+          <n-gi span="0 425:2 705:3" />
+        </n-grid>
+      </n-card>
+      <div style="margin-top: 15px">
+        <n-card hoverable>
+          <!-- 菜单 -->
+          <TagMenu />
+          <n-divider />
+          <router-view />
+        </n-card>
+      </div>
+      <!-- README -->
+      <div v-if="showREADME" style="margin-top: 15px">
+        <READMECard />
+      </div>
+    </div>
     <n-global-style />
   </n-config-provider>
 </template>
@@ -58,11 +52,9 @@ import {
   NGrid,
   NInput,
   NButton,
-  NLayout,
   NBackTop,
   NDivider,
   NInputGroup,
-  NLayoutContent,
 } from "naive-ui";
 import { zhCN, darkTheme, NGlobalStyle, NConfigProvider } from "naive-ui"; // NaiveUI Config
 
