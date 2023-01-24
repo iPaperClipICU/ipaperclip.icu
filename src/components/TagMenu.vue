41e0ca37e8fa001607b0d4a5c53eff52c88c0db6
qiaoshouzi <qiaoshouzi@hgy.ooo>
2023-01-24 20:18:41 +0800

    Update TagMenu.vue

--- src/components/TagMenu.vue
+++ src/components/TagMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <n-space justify="center" :size="30">
-    <div v-for="(item, index) in menuData" :key="index">
+    <div v-for="(item, index) in data.menuData" :key="index">
       <n-dropdown
         v-if="item.length === 2"
         trigger="hover"
@@ -30,6 +30,7 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { NSpace, NButton, NDropdown } from "naive-ui";
 
 import router from "@/router";
@@ -39,7 +40,10 @@ import { useCounterStore } from "@/stores/counter";
 const data = getData();
 const counter = useCounterStore();
 
-const menuData = data.menuData;
+router.beforeEach((to) => {
+  const p = to.params.pathMatch;
+  if (p === undefined || p === "") counter.AtPageFilesName = "";
+});
 
 const getDropdownOptions = (data: [string, string[]]) => {
   const options = [];
