8fb174ba6ae22105c353560dba10b8c4200dc207
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-02-02 14:08:18 +0800

    优化 945 - 1140 屏幕下  Tag 显示样式

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <n-config-provider :locale="zhCN" :theme="darkTheme">
     <n-back-top />
-    <n-layout position="absolute">
-      <n-layout-header
-        bordered
-        class="navigation"
-        style="height: var(--header-height)"
-      >
-        <HeadView
-          @change-padding="(key: string) => navigationCSS_padding = key"
-        />
+    <n-layout
+      position="absolute"
+      :style="{ '--header-height': showTag ? '98px' : '64px' }"
+    >
+      <n-layout-header bordered class="navigation">
+        <div class="navigation-head">
+          <HeadView
+            @change-padding="(key: string) => navigationCSS_padding = key"
+            @set-tag="(key: boolean) => showTag = key"
+          />
+        </div>
+        <n-collapse-transition :show="showTag">
+          <div
+            class="navigation-tag"
+            :style="{ display: showTag ? 'flex' : 'none' }"
+          >
+            <TagMenu />
+          </div>
+        </n-collapse-transition>
       </n-layout-header>
       <n-layout-content
         :native-scrollbar="false"
@@ -37,14 +47,17 @@ import {
   NBackTop,
   NLayoutHeader,
   NLayoutContent,
+  NCollapseTransition,
 } from "naive-ui";
 import { zhCN, darkTheme, NGlobalStyle, NConfigProvider } from "naive-ui"; // NaiveUI Config
 
 import router from "@/router";
 import { clearRubbish, getFileInfo } from "@/assets/utils.js";
+import TagMenu from "./components/TagMenu.vue";
 import HeadView from "@/components/HeadView.vue";
 import READMECard from "./components/READMECard.vue";
 
+const showTag = ref<boolean>(false);
 const showREADME = ref<boolean>(true);
 const navigationCSS_padding = ref<string>("32px");
 
@@ -63,13 +76,20 @@ router.beforeEach((to, from) => {
 </script>
 
 <style>
-body {
-  --header-height: 64px;
-}
-
 .navigation {
-  display: flex;
+  display: grid;
+  border-radius: 0.5em;
+  height: var(--header-height);
   padding: 0 v-bind(navigationCSS_padding);
+  padding-top: 15px;
+}
+.navigation-head {
+  display: flex;
+  height: 34px;
+}
+.navigation-tag {
+  height: 34px;
+  justify-content: center;
 }
 
 .container {
