6c4fe29d55cab81713b319e8af093d845d0c048a
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-03-17 22:38:55 +0800

    Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import * as Sentry from "@sentry/vue";
+
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+if (import.meta.env.PROD) {
+  const { BrowserTracing } = require("@sentry/browser");
+
+  Sentry.init({
+    app,
+    dsn: "https://2956ca4446814699b4025c0930d18ee1@o4504849146118144.ingest.sentry.io/4504854292004864",
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracePropagationTargets: [
+          "ipaperclip-file.xodvnm.cn",
+          "cf.ipaperclip-icu.cyou",
+          "r2.ipaperclip-icu.cyou",
+          "api.ipaperclip-icu.cyou",
+          "api.ipaperclip.icu",
+          /^\//,
+        ],
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 app.use(createPinia());
 app.use(router);
