7467f86247808b889a4aaf2d2dd2759512a8537a
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-06-04 21:40:16 +0800

    Remove @vercel/analytics

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,9 @@ import { BrowserTracing } from "@sentry/tracing";
 import App from "./App.vue";
 import router from "./router";
 
-import { inject as VercelAnalyticsInject } from "@vercel/analytics";
-
 const app = createApp(App);
 
 if (import.meta.env.PROD) {
-  VercelAnalyticsInject();
   Sentry.init({
     app,
     dsn: "https://ae9c3808da514430acdf87f18b2a02e7@o4504849146118144.ingest.sentry.io/4504849148608512",
