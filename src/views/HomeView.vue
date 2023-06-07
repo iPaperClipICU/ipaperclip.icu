511aeaf1742b22956c4e40ad6dbf18dbcb25c982
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-06-08 00:59:45 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,7 @@
 <template>
-  <!-- <n-card hoverable>
-    <n-empty description="请在「菜单」中选择" />
-  </n-card> -->
-  <!-- README -->
   <READMECard />
 </template>
 
 <script setup lang="ts">
-// import { NCard, NEmpty } from "naive-ui";
 import READMECard from "@/components/READMECard.vue";
 </script>
