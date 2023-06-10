c44e181c1e39f3b34bd64db084a8310328f4b64b
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-06-10 14:58:27 +0800

    Add @sentry/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 import { createHtmlPlugin } from "vite-plugin-html";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -14,6 +15,9 @@ export default defineConfig(({ command }) => {
       ? "" // 开发
       : `<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "a9d6db727c5a4b3483f3bb80358921ed"}'></script>`; // 生产
   return {
+    build: {
+      sourcemap: true,
+    },
     server: {
       host: "0.0.0.0",
       https: needSSL,
@@ -32,6 +36,14 @@ export default defineConfig(({ command }) => {
       {
         ...basicSsl(),
         apply: needSSL ? "serve" : command === "serve" ? "build" : "serve",
+      },
+      {
+        ...sentryVitePlugin({
+          org: "q-team-wl",
+          project: "ipaperclip-icu",
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+        }),
+        apply: "build",
       },
     ],
     resolve: {
