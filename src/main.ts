e82b50a3de597c1e8b0999eb2a9ee8c28e6b529d
qiaoshouzi <qiaoshouzi@cfm.moe>
2024-04-12 22:27:36 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ if (import.meta.env.PROD) {
         app,
         dsn: "https://ae9c3808da514430acdf87f18b2a02e7@o4504849146118144.ingest.sentry.io/4504849148608512",
         tracesSampleRate: 1.0,
+        release: import.meta.env.CF_PAGES_COMMIT_SHA,
       });
     });
   })();
